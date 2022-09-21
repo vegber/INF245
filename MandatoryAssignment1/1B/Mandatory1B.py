f05ea13fbac8fa42c45ff882bb194b71665f7b92
vbe020 <vbe020@uib.no>
2022-09-21 12:54:27 +0200

    not done

--- MandatoryAssignment1/1B/Mandatory1B.py
+++ MandatoryAssignment1/1B/Mandatory1B.py
@@ -5,6 +5,7 @@ modulus a prime number p.
 
 import re
 from turtle import end_fill
+from typing import Any
 
 
 def Polynomial_addition(f_x: list, g_x: list, p: int) -> list:
@@ -39,60 +40,77 @@ def normalize(poly):
         poly.append(0)
 
 
-def Polynomial_division(num, den):
-    # Create normalized copies of the args
-    num = num[:]
-    normalize(num)
-    den = den[:]
-    normalize(den)
+def degree(poly):
+    while poly and poly[-1] == 0:
+        poly.pop()  # normalize
+    return len(poly) - 1
 
-    if len(num) >= len(den):
-        # Shift den towards right so it's the same degree as num
-        shiftlen = len(num) - len(den)
-        den = [0] * shiftlen + den
+
+def poly_div(N: list, D: list, p: int):
+    dD = degree(D)
+    dN = degree(N)
+    if dD < 0: raise ZeroDivisionError
+    if dN >= dD:
+        q = [0] * (dN - 1)
+        while dN >= dD:
+            d = [0] * (dN - dD) + D
+            mult = q[dN - dD] = modDivide(N[-1], d[-1], p)  # N[-1] / float(d[-1])
+            d = [(coeff * mult) % p for coeff in d]
+            N = [(coeffN - coeffd) % p for coeffN, coeffd in zip(N, d)]
+            dN = degree(N)
+        r = N
     else:
-        return [0], num
+        q = [0]
+        r = N
+    return q, r
 
-    quot = []
-    divisor = float(den[-1])
-    for i in range(shiftlen + 1):
-        # Get the next coefficient of the quotient.
-        mult = num[-1] / divisor
-        quot = [mult] + quot
 
-        # Subtract mult * den from num, but don't bother if mult == 0
-        # Note that when i==0, mult!=0; so quot is automatically normalized.
-        if mult != 0:
-            d = [mult * u for u in den]
-            num = [u - v for u, v in zip(num, d)]
+# Function to compute a/b under modulo m
+def modDivide(a: int, b: int, m: int) -> Exception | int | float | Any:
+    a = a % m
+    inv = modInverse(b, m)
+    if inv == -1:
+        return 0
+    else:
+        return (inv * a) % m
 
-        num.pop()
-        den.pop(0)
 
-    normalize(num)
-    return quot, num
+def modInverse(b: int, m: int) -> int:
+    import math
+    g = math.gcd(b, m)
+    if g != 1:
+        # print("Inverse doesn't exist")
+        return -1
+    else:
+        # If b and m are relatively prime,
+        # then modulo inverse is b^(m-2) mode m
+        return pow(b, m - 2, m)
 
 
 def PolyDiv(f_x, g_x, p: int):
     import copy
     # make sure deg(f_x) > deg(g_x)
     assert len(f_x) >= len(g_x)
-
+    g_x = [0] * (len(f_x) - len(g_x)) + g_x
     # Copy input arr. so we can change them
     f_x = copy.deepcopy(f_x)
     g_x = copy.deepcopy(g_x)
 
     i_th_elem = 0
+    # find index of element with the highest order != 0
+    highest_order_elemt_index = [i for i, e in enumerate(f_x) if e != 0][0]
+    order_of_highest_elemt = len(f_x) - highest_order_elemt_index
     while True:
-        dividator = f_x[i_th_elem]
+        if order_of_highest_elemt < len(g_x):
+            # can't divide this element, rest is remainder
+            return
+        # find a s.t a*g[0] == inv(f_x(0)) = 0
+        theta = modDivide(f_x[highest_order_elemt_index], g_x[1], p)
 
-        divident = g_x[0]
+        f_x = [(f + (modInverse((theta * g), p))) % p for f, g in zip(f_x, g_x)]
 
-        # mult. divident with x s.t dividator == 0 mod p
-        print(divident)
-        faktor = (divident % p) * pow(divident, -1, p)
-        print(faktor)
-        print(dividator * faktor % p)
+        print(f_x)
+
         break
 
 
@@ -112,7 +130,11 @@ if __name__ == '__main__':
     a = [1, 0, 3, 1]
     b = [1, 2, 1]
 
-    PolyDiv(a, b, PRIME)
-
+    print("POLYNOMIAL LONG DIVISION")
+    N = [4, 2, 1]
+    D = [0, 2, 2]
+    print(PolyDiv(a, b, PRIME))
+    # print(" %s / %s =" % (N, D))
+    # print(" %s remainder %s" % poly_div(N, D, 5))
     # print(Polynomial_addition(list_1, list_2, PRIME))
     # PrintPolynomial(Polynomial_multiplication(list_1, list_2, PRIME))
