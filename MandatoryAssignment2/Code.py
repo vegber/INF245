7bba01d6bbfd2d3f6f57d26a60d9c1e3031d9f67
Vegard Berge <Vegard.Berge@knowit.no>
2022-10-06 13:19:07 +0200

    Dixon's step one finished.

--- MandatoryAssignment2/Code.py
+++ MandatoryAssignment2/Code.py
@@ -1,4 +1,5 @@
 import math
+import random
 from typing import Any
 import time
 from fractions import Fraction
@@ -110,22 +111,118 @@ def FactorByP_Method(N: int) -> str | int | Any:
         return d
 
 
-def DixonsMethod():
+def is_prime(n):
+    """
+    :param n:
+    :return: True/False n is prime
+    """
+    for i in range(2, n):
+        if (n % i) == 0:
+            return False
+    return True
+
+
+def createBsmooth(B: int):
+    """
+    Find primes in B
+    :param B:
+    :return: list(primes_n)
+    """
+    return [p for p in range(2, B + 1) if is_prime(p)]
+
+
+def prime_factors(n):
+    i = 2
+    factors = []
+    while i * i <= n:
+        if n % i:
+            i += 1
+        else:
+            n //= i
+            factors.append(i)
+    if n > 1:
+        factors.append(n)
+    return sorted(factors)
+
+
+def is_b_smooth(b: list, S_b: list) -> bool:
+    return max(b) <= max(S_b)
+
+
+def DixonsMethod(N: int, B: int):
+    """
+    Dixon's algorithm, Random squares:
+
+    :param N: N is the number to factor
+    :param B: B is the smoothness / complexity
+    :return: Factors of N / exl. trivial solutions
+    """
+
+    ### STEP 1 ###
+    # Random 1 ≤ x < N
+    # compute b ≡ x^2 mod N, 1 ≤ b < N
+    # trial division by primes |S_b|
+    # |S_b| = {2, 3 .... ≤ B}
+
+    # if b i s B - smooth
+    # Yes: store factor of trial div. given by b = PI from j=1 to n, q_j ^(lj), q_j is in set S_b
+    # else repeat new x
+
+    S_b = createBsmooth(B)
+    n = len(S_b)
+    random_X_s = []
+    while len(random_X_s) != B:
+        # find random b
+        x_s = random.randint(2, N)
+        # compute conruence
+        b = (x_s ** 2) % N
+        if is_prime(b) or b == 1: continue
+        # find factors
+        factors_of_b = prime_factors(b)
+        # check if valid
+        if is_b_smooth(factors_of_b, S_b): random_X_s.append((b, factors_of_b))
+
+    print(random_X_s)
+
+    ### STEP 2 ###
+    # collect m = n * c, rows. ( create matrix)
+
+    ### STEP 3 ###
+    # Contruct system of lin. equations, i.e matrix
+
+    ### STEP 4 ###
+    # Solve, system of lin. equations: i.e matrix - mod 2
+
+    ### STEP 5 ###
+    # set X = x_1^(Z_1), ... x_m^(Z_m) mod N
+    # set Y = q_1^(k_1), ... q_n^(K_m) mod N
+
+    ### STEP 6 ###
+    # GCD(X - Y, N) == N_1
+    # if N_1 == 1 or N
+    # Try another solution of step 3
+    #
+    # N_1 != N: N_1 is factor of N!
+
     pass
 
 
 if __name__ == '__main__':
-    N = 1098667602555738997359355609545017255443451483195436981354791765341639135658156206242197992115989996829728203054347117299
-    e = 415884005149779743103130951097941968793336745983309971301432658775763996247677181243042840232106535367251782466233724389
+    # N = 1098667602555738997359355609545017255443451483195436981354791765341639135658156206242197992115989996829728203054347117299
+    # e = 415884005149779743103130951097941968793336745983309971301432658775763996247677181243042840232106535367251782466233724389
 
     # e = 42667  # 388511
     # N = 64741  # 331879
-    a, b = ContinuedFractionAlgorithm(N, e)
-    print(a, b)
-    p_method_N = 10862216162096506735513546937
+    # a, b = ContinuedFractionAlgorithm(N, e)
+    # print(a, b)
+    # p_method_N = 10862216162096506735513546937
 
     # var = ContinuedFractionAlgorithm(int(N), int(e))
     # print(continued_fraction(int(N) / int(e)))
     # start = time.time()
     # p_method_factor = FactorByP_Method(p_method_N)
     # print(f"Time took: {time.time() - start} and result was: {p_method_factor}")
+
+    N = 12
+    DixonsMethod(100, 7)
+    # print(prime_factors(N))
