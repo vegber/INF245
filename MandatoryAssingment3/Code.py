cd5fb56e73ee58bd5603cd1fe13a034f12cbd56a
Vegard <Vegard.berge8@gmail.com>
2022-10-30 15:21:59 +0100

    alles gut, index calc finito

--- MandatoryAssingment3/Code.py
+++ MandatoryAssingment3/Code.py
@@ -318,7 +318,12 @@ def SolveRowEchelonForm(m: list, p: int):
     return M
 
 
-def IndexCalculusAlgorithmj(p=2602163, alfa=1535637, B=30, g=2):
+def printMatrix(b):
+    for lmao in b:
+        print(list(lmao), end="\n")
+
+
+def IndexCalculusAlgorithmj(p=2599739, alfa=1629414, B=30, g=2):
     S_b, m, n, random_X_s = FindRowsBsmooth(B, p)
 
     # STEP 2
@@ -327,29 +332,33 @@ def IndexCalculusAlgorithmj(p=2602163, alfa=1535637, B=30, g=2):
     out = None
     while out is None:
         try:
-            out = rref_mod_n(random_X_s, p - 1)
-            a = np.array(out, dtype=int)
-            b = a.reshape(m, 11)
-            # print(b)
-
             while True:
-                y = random.randint(0, p - 1)
-                tmp = (alfa * (pow(g, y))) % p
-                fac_ = prime_factors(tmp)
-                if is_b_smooth(fac_, S_b):
-                    break
+                out = rref_mod_n(random_X_s, p - 1)
+                a = np.array(out, dtype=int)
+                matrix = a.reshape(m, 11)
 
-            # solve:
-            l = [0] * n
-            for i_s in range(len(S_b)):
-                l[i_s] = fac_.count(S_b[i_s])
-            x_vals = findXi(b)
-            x = 0
-            for x_i, l_i in zip(x_vals, l):
-                x += (x_i * l_i)
-            x -= y
-            x = x % p
-            return x
+                if matrix[0][-1] == 0:
+                    S_b, m, n, random_X_s = FindRowsBsmooth(B, p)
+                else:
+                    printMatrix(matrix)
+                    while True:
+                        y = random.randint(0, p - 1)
+                        b = (pow(g, y, p)*alfa) % p
+                        fac_ = prime_factors(b)
+                        if is_b_smooth(fac_, S_b):
+                            break
+
+                    # solve:
+                    l = [0] * n
+                    for i_s in range(len(S_b)):
+                        l[i_s] = fac_.count(S_b[i_s])
+                    x_vals = findXi(matrix)
+                    x = 0
+                    for x_i, l_i in zip(x_vals, l):
+                        x += x_i * l_i # (x_i**l_i)
+                    x = (x - y) % (p-1)
+
+                    return x
         except:
             S_b, m, n, random_X_s = FindRowsBsmooth(B, p)
 
@@ -362,9 +371,9 @@ def FindRowsBsmooth(B, p):
     m = n
     while len(random_X_s) != m:
         # find random b
-        x_s = random.randint(math.floor(math.sqrt(p)), p)
+        x_s = random.randint(math.floor(math.sqrt(p)), p-1)
         # compute congruence
-        b = (x_s ** 2) % p
+        b = pow(2, x_s, p) # (x_s ** 2) % p
         if b == 1:
             continue  # if prime and >= B ??
         # find factors
@@ -394,13 +403,15 @@ def rundTaskThree():
     # should be 2116767
     while True:
 
-        x = int(IndexCalculusAlgorithmj())
-        if pow(2, x, 2602163) == 1535637 or pow(2, x, 2602163 - 1) == 1535637:
-            print(f"Found correct x: {x}")
-            break
-        else:
-            print(f"Wrong: got x ==  {x}", end="\n")
-            print()
+        x = IndexCalculusAlgorithmj()
+        if x is not None:
+            x = int(x)
+            if pow(2, x, 2599739) == 1629414 or pow(2, x, 2599739 - 1) == 1629414:
+                print(f"Found correct x: {x}")
+                break
+            else:
+                print(f"Wrong: got x ==  {x}", end="\n")
+                print()
 
 
 if __name__ == '__main__':
